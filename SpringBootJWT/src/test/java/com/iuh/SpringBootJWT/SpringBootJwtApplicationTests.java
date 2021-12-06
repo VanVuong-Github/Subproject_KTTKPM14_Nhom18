7ceaf1c4265b44427a9e8d7f6e5cfd93baa52d16
VanVuong <lai.vanvuong2866@gmail.com>
2021-12-06 10:51:33 +0700

    commit new - add jwt authentication project

---
+++ SpringBootJWT/src/test/java/com/iuh/SpringBootJWT/SpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.SpringBootJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
