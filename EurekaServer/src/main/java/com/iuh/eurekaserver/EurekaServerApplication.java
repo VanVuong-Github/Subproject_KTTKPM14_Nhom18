eec71fb7c5151b2932f64573aa0ff3c70bf346ec
VanVuong <lai.vanvuong2866@gmail.com>
2021-12-08 11:57:24 +0700

    commit fix

--- EurekaServer/src/main/java/com/iuh/eurekaserver/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/iuh/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.iuh.eurekaserver;
+package com.iuh.EurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
 
 }
