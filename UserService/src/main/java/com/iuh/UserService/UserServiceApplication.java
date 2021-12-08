7ed36e6ce316b4b5db07c9cfe7f91f3ff0be6e49
Your Name <you@example.com>
2021-12-08 21:11:30 +0700

    cloud-config and swagger ui update

--- UserService/src/main/java/com/iuh/UserService/UserServiceApplication.java
+++ UserService/src/main/java/com/iuh/UserService/UserServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSwagger2
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
