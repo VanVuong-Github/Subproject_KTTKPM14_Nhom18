92b552c51c2200e0fd5d27a704696e20ebf4f33d
Your Name <you@example.com>
2021-12-12 14:45:35 +0700

    config-server

--- EurekaServer/src/main/java/com/iuh/eurekaserver/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/iuh/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.iuh.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//@EnableConfigServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
--- SpringBootJWT/src/main/java/com/iuh/SpringBootJWT/SpringBootJwtApplication.java
+++ SpringBootJWT/src/main/java/com/iuh/SpringBootJWT/SpringBootJwtApplication.java
@@ -2,6 +2,7 @@ package com.iuh.SpringBootJWT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableMongoAuditing
 @EnableSwagger2
+@EnableConfigServer
 public class SpringBootJwtApplication {
 
 	@Bean
